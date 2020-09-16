02047e6435595585e4846e6acc0128d25a1f7aff
Shahzeb Khan <shahzeb3939@gmail.com>
2020-09-16 13:27:24 +0500

    after saving Cast to excel file

--- support/action/Excel/saveCastToExcel.js
+++ support/action/Excel/saveCastToExcel.js
@@ -1,15 +1,27 @@
-import googlePage from "../../../page/googlePage"
+import xlsx from "xlsx"
 
 export default () => {
     var jArray = [];
     var jObj = {};
 
+    var wb = xlsx.readFile("qaautomation.xlsx");
+
+    var ws = wb.Sheets["Series Cast"];
+
+
     for (var i=2; i<=98; i+=2) {
         jObj = {};
+
         jObj["Name"] = $(`tr:nth-child(${i}) > td:nth-child(2) > a`).getText();
-        jObj["Screen Name"] = $(`tr:nth-child(${i}) > td.character > a:nth-child(1)`).getText();
+
+        jObj["Screen Name"] = $(`tr:nth-child(${i}) > td.character`).getText().split("\n")[0].split(" ")[1];
+        
         jObj["Appearances"] = $(`tr:nth-child(${i}) > td.character > a.toggle-episodes`).getText();
+
         jArray.push(jObj);
     }
-    console.log(jArray);
+
+    var nws = xlsx.utils.json_to_sheet(jArray);
+    xlsx.utils.book_append_sheet(wb,nws,"Series Cast");
+    xlsx.writeFile(wb, "qaautomation.xlsx")
 }
